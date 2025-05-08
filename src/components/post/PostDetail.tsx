0507ab30d67615424c94f1e29f4229e53013dbd4
seoo2001 <seoo2001@naver.com>
2025-05-08 17:14:42 +0900

    edit code block style

--- src/components/post/PostDetail.tsx
+++ src/components/post/PostDetail.tsx
@@ -6,13 +6,77 @@ import rehypeSlug from 'rehype-slug';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
 import 'katex/dist/katex.min.css';
+import rehypePrettyCode from 'rehype-pretty-code';
 
 interface PostDetailProps {
     post: Post;
 }
 
 export const PostDetail = ({ post }: PostDetailProps) => {
-
+    const customTheme = {
+        "name": "my-custom-theme",
+        "type": "dark",
+        "colors": {
+          "editor.background": "var(--code-background)",
+          "editor.foreground": "var(--code-foreground)"
+        },
+        "tokenColors": [
+          {
+            "scope": "constant",
+            "settings": {
+              "foreground": "var(--code-token-constant)"
+            }
+          },
+          {
+            "scope": "string",
+            "settings": {
+              "foreground": "var(--code-token-string)"
+            }
+          },
+          {
+            "scope": "comment",
+            "settings": {
+              "foreground": "var(--code-token-comment)"
+            }
+          },
+          {
+            "scope": "keyword",
+            "settings": {
+              "foreground": "var(--code-token-keyword)"
+            }
+          },
+          {
+            "scope": "parameter",
+            "settings": {
+              "foreground": "var(--code-token-parameter)"
+            }
+          },
+          {
+            "scope": "function",
+            "settings": {
+              "foreground": "var(--code-token-function)"
+            }
+          },
+          {
+            "scope": "string.expression",
+            "settings": {
+              "foreground": "var(--code-token-string-expression)"
+            }
+          },
+          {
+            "scope": "punctuation",
+            "settings": {
+              "foreground": "var(--code-token-punctuation)"
+            }
+          },
+          {
+            "scope": "link",
+            "settings": {
+              "foreground": "var(--code-token-link)"
+            }
+          }
+        ]
+      }
 
     return (
         <div className="post-detail">
@@ -28,6 +92,7 @@ export const PostDetail = ({ post }: PostDetailProps) => {
                         [rehypeSlug, { behavior: 'wrap' }],
                         [rehypeAutolinkHeadings, { behavior: 'wrap' }],
                         [rehypeKatex, { output: 'htmlAndMathml', throwOnError: false }],
+                        [rehypePrettyCode, { theme: customTheme }]
                     ],
                 },
                 }}
