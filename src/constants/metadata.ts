9ff66c78d727d00759b2472f86f400e6f36287af
seoo2001 <seoo2001@naver.com>
2025-05-15 02:09:30 +0900

    edit verification code

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -15,5 +15,5 @@ export const META = {
     url: 'https://www.ilez.xyz',
     ogImage: '/opengraph.png',
     googleVerification: "-zitLknPv5kRbjZ4Mg8Dkjveiak80WHCT-clix5QThI",
-    naverVerification: "e887c89d334e6ce8c7d4f457f5671ae820a0da2e",
+    naverVerification: "7a8835b3bcf06766aeb89a81adc8fbfffd76706f",
 }
