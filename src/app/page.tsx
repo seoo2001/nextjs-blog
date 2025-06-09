8ad114e6e575f2cd9a3126d86102607a4dd074c5
seoo2001 <seoo2001@naver.com>
2025-06-09 21:31:17 +0900

    home 문구 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <Header title="SEO. DongJoon." />
       <div className="mt-3 min-h-[calc(100vh-var(--page-top)-240px)]">
         <p>
-          Hello, I'm DongJoon. I'm a software engineer.
+          Hello, I&apos;m DongJoon. I&apos;m a software engineer.
         </p>
       </div>
     </>
